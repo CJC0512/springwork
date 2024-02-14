55bd1db674b762380f1ec1589317a145db33d65b
CJC0512 <ehdchs2006@gmail.com>
2024-02-14 15:50:57 +0900

    chap05 sec01

---
+++ chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/ContextConfiguration.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section01.aop;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class ContextConfiguration {
+
+}
